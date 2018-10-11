e66eebf23c919148cb5832b7b57e58e5a2a68eb0
yaoleic <32326164+yaoleic@users.noreply.github.com>
2018-10-11 14:17:27 +0800

    t

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -12,11 +12,12 @@
 */
 //文章列表页
  Route::get('/posts','\App\Http\Controllers\PostController@index');
- //文章详情页
- Route::get('/posts/{post}','\App\Http\Controllers\PostController@show');
+
  //创建文章
  Route::get('/posts/create','\App\Http\Controllers\PostController@create');
  Route::post('/posts','\App\Http\Controllers\PostController@store');
+//文章详情页
+Route::get('/posts/{post}','\App\Http\Controllers\PostController@show');
  //编辑文章
  Route::get('/posts/{post}/edit','\App\Http\Controllers\PostController@edit');
  Route::put('/posts/{post}','\App\Http\Controllers\PostController@update');
